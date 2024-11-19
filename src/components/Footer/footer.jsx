2a3d5b12fa202bd0c43721fc52ad6a5750071685
Nataly-Costaa <natalynaty653@gmail.com>
2024-11-19 15:00:56 -0300

    :iphone: Adicionando responsividade

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,7 +3,18 @@
 export default function footer() {
     return (
         <footer>
+            {/* <section id={S.contacts}>
+                <p id={S.number}>4002-8922</p>
+
+                <nav id={S.networks}>
+                    <img src={} alt="" />
+                    <img src={} alt="" />
+                    <img src={} alt="" />
+                    <img src={} alt="" />
+                </nav>
+            </section> */}
             <h1>Footer</h1>
+        
         </footer>
     );
 };
