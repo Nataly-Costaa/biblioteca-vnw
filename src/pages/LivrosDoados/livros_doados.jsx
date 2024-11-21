1e4294195d7018e39af478e03ec3fe4abace11a1
Nataly-Costaa <natalynaty653@gmail.com>
2024-11-20 22:09:10 -0300

    :sparkles: feat: Página de livros doados

--- src/pages/LivrosDoados/livros_doados.jsx
+++ src/pages/LivrosDoados/livros_doados.jsx
@@ -1,5 +1,22 @@
+import S from "./livros_doados.module.scss";
+import book1 from "../../assets/img/book_harry_potter.png";
+
 export default function quero_doar() {
     return (
-        <h1>Quero Doar</h1>
+        <main id={S.pageLivrosDoados}>
+            <h2>Livros doados</h2>
+
+            <section id={S.container3}>
+                <div className={S.cardBooks}>
+                    <img src={book1} alt="Livro do Harry Potter" />
+
+                    <div id={S.bookInfo}>
+                        <p id={S.nameBook}>Harry Potter e a Pedra Filosofal</p>
+                        <p>J.K. Rowling</p>
+                        <p>Fantasia</p>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
